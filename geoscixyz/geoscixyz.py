5c44273f8cd524004b790fcc2fa42f80a84e4d0d
Lindsey Heagy <lindseyheagy@gmail.com>
2016-12-20 18:24:07 -0800

    start of presentations on geosci

--- geoscixyz/geoscixyz.py
+++ geoscixyz/geoscixyz.py
@@ -14,9 +14,12 @@ import json
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__).split('/')[:-1])),
+    loader=jinja2.FileSystemLoader(
+        os.path.join(os.path.dirname(__file__).split('/')[:-1])
+    ),
     extensions=['jinja2.ext.autoescape'],
     autoescape=False)
+
 
 def setTemplate(self, template_values, templateFile):
     _templateFolder = 'templates/'
@@ -30,16 +33,20 @@ def setTemplate(self, template_values, templateFile):
     self.response.write(template.render(template_values))
 
 
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
 
-        setTemplate(self, {"indexPage":True}, 'index.html')
+        setTemplate(self, {"indexPage": True}, 'index.html')
 
 
 class Why(webapp2.RequestHandler):
     def get(self):
         setTemplate(self, {}, 'why.html')
+
+
+class Presentations(webapp2.RequestHandler):
+    def get(self):
+        setTemplate(self, {}, 'presentations.html')
 
 class Who(webapp2.RequestHandler):
     def get(self):
@@ -48,18 +55,18 @@ class Who(webapp2.RequestHandler):
 
 class Contact(webapp2.RequestHandler):
     def get(self, mailSent=False):
-        data = {'mailSent':mailSent}
+        data = {'mailSent': mailSent}
         setTemplate(self, data, 'contact.html')
 
     def post(self):
-        email   = self.request.get('email')
-        name    = self.request.get('name')
+        email = self.request.get('email')
+        name = self.request.get('name')
         message = self.request.get('message')
 
         sender_address = "GeoSci Mail <lindseyheagy@gmail.com>"
         email_to = "Lindsey Heagy <lindseyheagy@gmail.com>"
         email_subject = "GeoSci Mail"
-        email_message = "New email from:\n\n%s<%s>\n\n\n%s\n" % (name,email,message)
+        email_message = "New email from:\n\n%s<%s>\n\n\n%s\n" % (name, email, message)
 
         mail.send_mail(sender_address, email_to, email_subject, email_message)
         self.get(mailSent=True)
@@ -75,12 +82,11 @@ class Error(webapp2.RequestHandler):
         setTemplate(self, {}, 'error.html')
 
 
-
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    # ('/journal', Thoughts),
     ('/why', Why),
     ('/who', Who),
+    ('/presentations', Presentations),
     ('/img/.*', Images),
     ('/contact', Contact),
     ('/.*', Error),
